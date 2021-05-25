0135d182cce433ddcee77c077b9bc865dce1132a
yeswhos <yeswhos@outlook.com>
2021-05-25 14:01:47 +0800

    0525 144

---
+++ Tree/144_Easy.py
@@ -0,0 +1,25 @@
+"""
+2021.05.25
+mengfanhui
+"""
+
+# 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+# 主要思路就是用递归。
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def preorderTraversal(self, root: TreeNode):
+        res = []
+        def preOrder(node):
+            if node is None:
+                return
+            res.append(node.val)
+            preOrder(node.left)
+            preOrder(node.right)
+        preOrder(root)
+        return res
